1341952c1748c410144139e47134f7897bd25d1e
Sergej Derjabkin <sergej@sendev.de>
2018-07-17 09:49:13 +0200

    - Added OneTimeMethod functionality

--- src/CommonVersion.cs
+++ src/CommonVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyVersion("1.0.13.0")]
---
+++ src/ObjectScripting/Attributes/OneTimeMethodAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SenDev.XafTools.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class OneTimeMethodAttribute : Attribute
+    {
+    }
+}
